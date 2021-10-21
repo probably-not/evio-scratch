5f96c9d9dfcfbcc619363b16f95899bf29c79a71
Coby Benveniste <coby.b@arpeely.com>
2021-10-21 08:36:07 +0300

    Add handler implementation

---
+++ internal/handler.go
@@ -0,0 +1,71 @@
+package internal
+
+import (
+	"fmt"
+
+	"github.com/tidwall/evio"
+)
+
+func NewHandler(loops, port int) evio.Events {
+	var handler evio.Events
+	handler.NumLoops = loops
+	handler.LoadBalance = evio.RoundRobin
+
+	handler.Serving = func(server evio.Server) evio.Action {
+		fmt.Println("evio server started with", server.NumLoops, "event loops on port", port)
+		return evio.None
+	}
+
+	handler.Opened = func(c evio.Conn) ([]byte, evio.Options, evio.Action) {
+		fmt.Println("new connection opened between", c.LocalAddr(), "and", c.RemoteAddr())
+		c.SetContext(&evio.InputStream{})
+		return nil, evio.Options{}, evio.None
+	}
+
+	handler.Closed = func(c evio.Conn, err error) evio.Action {
+		fmt.Println("connection between", c.LocalAddr(), "and", c.RemoteAddr(), "has been closed with error value", err)
+		return evio.None
+	}
+
+	handler.Data = func(c evio.Conn, in []byte) ([]byte, evio.Action) {
+		fmt.Println("connection between", c.LocalAddr(), "and", c.RemoteAddr(), "received data", string(in))
+		return nil, evio.None
+	}
+
+	return handler
+}
+
+/*
+	events.Data = func(c evio.Conn, in []byte) (out []byte, action evio.Action) {
+		if in == nil {
+			return
+		}
+		is := c.Context().(*evio.InputStream)
+		data := is.Begin(in)
+		if noparse && bytes.Contains(data, []byte("\r\n\r\n")) {
+			// for testing minimal single packet request -> response.
+			out = appendresp(nil, "200 OK", "", res)
+			return
+		}
+		// process the pipeline
+		var req request
+		for {
+			leftover, err := parsereq(data, &req)
+			if err != nil {
+				// bad thing happened
+				out = appendresp(out, "500 Error", "", err.Error()+"\n")
+				action = evio.Close
+				break
+			} else if len(leftover) == len(data) {
+				// request not ready, yet
+				break
+			}
+			// handle the request
+			req.remoteAddr = c.RemoteAddr().String()
+			out = appendhandle(out, &req)
+			data = leftover
+		}
+		is.End(data)
+		return
+	}
+*/
--- main.go
+++ main.go
@@ -1,7 +1,29 @@
+// Copyright 2017 Joshua J Baker. All rights reserved.
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file.
+
 package main
 
-import "fmt"
+import (
+	"flag"
+	"strconv"
+
+	"github.com/probably-not/evio-scratch/internal"
+	"github.com/tidwall/evio"
+)
 
 func main() {
-	fmt.Println("Hello there")
+	var port int
+	var loops int
+
+	flag.IntVar(&port, "port", 8080, "server port")
+	flag.IntVar(&loops, "loops", 1, "num loops")
+	flag.Parse()
+
+	handler := internal.NewHandler(loops, port)
+
+	err := evio.Serve(handler, "tcp://:"+strconv.Itoa(port))
+	if err != nil {
+		panic(err)
+	}
 }
