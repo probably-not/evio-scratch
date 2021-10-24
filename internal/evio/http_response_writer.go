974546c5c06742990fa037b6424a71df0063bd8d
Coby Benveniste <coby.b@arpeely.com>
2021-10-24 12:41:09 +0300

    add naive response writer

---
+++ internal/evio/http_response_writer.go
@@ -0,0 +1,49 @@
+package evio
+
+import (
+	"bytes"
+	"io"
+	"net/http"
+
+	"github.com/probably-not/evio-scratch/internal/ioutil"
+)
+
+// A very basic naive http.ResponseWriter implementation that attempts to write to an underlying http.Response.
+// This should be further extended in the future to ensure we are writing the correct Headers, protocols, and flags
+// to the http.Response.
+type responseWriter struct {
+	buf []byte
+	*http.Response
+}
+
+func NewResponseWriter() *responseWriter {
+	return &responseWriter{
+		Response: &http.Response{
+			ProtoMajor: 1,
+			ProtoMinor: 1,
+		},
+	}
+}
+
+func (rw *responseWriter) Header() http.Header {
+	return rw.Response.Header
+}
+
+func (rw *responseWriter) Write(data []byte) (int, error) {
+	if rw.StatusCode == 0 {
+		rw.WriteHeader(200)
+	}
+
+	rw.buf = append(rw.buf, data...)
+	return len(data), nil
+}
+
+func (rw *responseWriter) WriteHeader(statusCode int) {
+	rw.StatusCode = statusCode
+}
+
+func (rw *responseWriter) WriteToBuf(w io.Writer) error {
+	rw.Body = ioutil.NopCloser(bytes.NewReader(rw.buf))
+	rw.ContentLength = int64(len(rw.buf))
+	return rw.Response.Write(w)
+}
