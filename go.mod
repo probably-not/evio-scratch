e8f73c1dbbacd25e039b64da7d1f5ed5dd08618a
Coby Benveniste <coby.b@arpeely.com>
2021-10-24 17:28:04 +0300

    make gnet basic handler

--- go.mod
+++ go.mod
@@ -2,6 +2,17 @@ module github.com/probably-not/evio-scratch
 
 go 1.17
 
-require github.com/tidwall/evio v1.0.8
+require (
+	github.com/panjf2000/gnet v1.5.3
+	github.com/tidwall/evio v1.0.8
+)
 
-require github.com/kavu/go_reuseport v1.5.0 // indirect
+require (
+	github.com/kavu/go_reuseport v1.5.0 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/multierr v1.7.0 // indirect
+	go.uber.org/zap v1.19.1 // indirect
+	golang.org/x/sys v0.0.0-20211023085530-d6a326fbbf70 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
+)
---
+++ internal/loop/gnet/handler.go
@@ -0,0 +1,128 @@
+package gnet
+
+import (
+	"bufio"
+	"bytes"
+	"context"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/panjf2000/gnet"
+	internalHttp "github.com/probably-not/evio-scratch/internal/http"
+	"github.com/probably-not/evio-scratch/internal/loop"
+	"github.com/tidwall/evio"
+)
+
+type GnetHandler struct {
+	ctx         context.Context
+	loops, port int
+	httpHandler http.Handler
+	*gnet.EventServer
+}
+
+func NewGnetLoop(ctx context.Context, loops, port int, httpHandler http.Handler) *GnetHandler {
+	handler := GnetHandler{
+		ctx:         ctx,
+		loops:       loops,
+		port:        port,
+		httpHandler: httpHandler,
+		EventServer: &gnet.EventServer{},
+	}
+
+	return &handler
+}
+
+// OnInitComplete fires on server up (one time)
+func (h *GnetHandler) OnInitComplete(server gnet.Server) gnet.Action {
+	fmt.Println("evio server started with", server.NumEventLoop, "event loops on address", h.port)
+
+	select {
+	case <-h.ctx.Done():
+		return gnet.Shutdown
+	default:
+		return gnet.None
+	}
+}
+
+// OnOpened fires on opening new connections (per connection)
+func (h *GnetHandler) OnOpened(c gnet.Conn) ([]byte, gnet.Action) {
+	c.SetContext(&evio.InputStream{})
+
+	select {
+	case <-h.ctx.Done():
+		return nil, gnet.Close
+	default:
+		return nil, gnet.None
+	}
+}
+
+// OnClosed fires on closing connections (per connection)
+func (h *GnetHandler) OnClosed(c gnet.Conn, err error) gnet.Action {
+	if err != nil {
+		fmt.Println("connection between", c.LocalAddr(), "and", c.RemoteAddr(), "has been closed with error value", err)
+	}
+
+	select {
+	case <-h.ctx.Done():
+		return gnet.Shutdown
+	default:
+		return gnet.None
+	}
+}
+
+// React fires on data being sent to a connection (per connection, per data frame read)
+func (h *GnetHandler) React(in []byte, c gnet.Conn) ([]byte, gnet.Action) {
+	if len(in) == 0 {
+		return nil, gnet.None
+	}
+
+	stream := c.Context().(*evio.InputStream)
+	data := stream.Begin(in)
+
+	complete, err := loop.IsRequestComplete(data)
+	if err != nil {
+		fmt.Println("Uh oh, there was an error checking completeness?", err)
+		return nil, gnet.Close
+	}
+
+	stream.End(data)
+	if !complete {
+		return nil, gnet.None
+	}
+
+	req, err := http.ReadRequest(bufio.NewReader(bytes.NewReader(data)))
+	if err != nil {
+		fmt.Println("Uh oh, there was an error creating the request?", err)
+		return nil, gnet.Close
+	}
+
+	res := internalHttp.NewResponseWriter()
+	h.httpHandler.ServeHTTP(res, req)
+
+	buf := bytes.NewBuffer(nil)
+	err = res.WriteToBuf(buf)
+	if err != nil {
+		fmt.Println("Uh oh, there was an error writing the response?", err)
+		return nil, gnet.Close
+	}
+
+	select {
+	case <-h.ctx.Done():
+		return nil, gnet.Close
+	default:
+		// Reset the connection context to an empty input stream once we have completed a full request in order to
+		// ensure that the next request starts empty.
+		c.SetContext(&evio.InputStream{})
+		return buf.Bytes(), gnet.None
+	}
+}
+
+func (h *GnetHandler) Tick() (delay time.Duration, action gnet.Action) {
+	select {
+	case <-h.ctx.Done():
+		return time.Second, gnet.Shutdown
+	default:
+		return time.Second, gnet.None
+	}
+}
