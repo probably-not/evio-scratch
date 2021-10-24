ab525306a277344121a2ffe91c59bf0f696425ef
Coby Benveniste <coby.b@arpeely.com>
2021-10-24 21:32:46 +0300

    an attempt at fast content length parsing

--- internal/http/parser.go
+++ internal/http/parser.go
@@ -67,3 +67,57 @@ func IsRequestComplete(data []byte) (bool, error) {
 
 	return true, nil
 }
+
+func parseContentLength(clen []byte) (int, error) {
+	// If we are lower than 0 or greater than 9, then we aren't an integer.
+	if clen[0] < '0' || clen[0] > '9' {
+		return -1, errBadRequest
+	}
+
+	// Start at the highest order of magnitude,
+	length := 0
+	for i := len(clen) - 1; i >= 0; i-- {
+		// Error possibilities
+		if clen[i] < '0' || clen[i] > '9' {
+			return -1, errBadRequest
+		}
+		v := byteToIntJump(clen[i])
+
+		// Error possibilities
+		if v <= 0 {
+			return -1, errBadRequest
+		}
+
+		// Add the magnitude to the length
+		length += v * i
+	}
+
+	return length, nil
+}
+
+func byteToIntJump(b byte) int {
+	switch b {
+	case '0':
+		return 0
+	case '1':
+		return 1
+	case '2':
+		return 2
+	case '3':
+		return 3
+	case '4':
+		return 4
+	case '5':
+		return 5
+	case '6':
+		return 6
+	case '7':
+		return 7
+	case '8':
+		return 8
+	case '9':
+		return 9
+	default:
+		return -1
+	}
+}
